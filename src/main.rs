a0569d6b4d021a5ae729a2287e51d4fc5a7852ff
sehnsucht <tech@sehnsucht.top>
2024-12-02 23:26:39 +0800

    feat: 获取今天的公历和农历日期

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,12 @@ mod config;
 
 fn main() {
     let config = load_config("config.yml").unwrap();
-    
+    // 获取当前日期
+    let today = Local::now().naive_local().date();
+    let mut day = SolarDay::from_ymd(
+        today.year().try_into().unwrap(),
+        today.month().try_into().unwrap(),
+        today.day().try_into().unwrap(),
+    );
+    println!("今天是 公历 {} 农历 {}", day, day.get_lunar_day());
 }
